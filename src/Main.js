21641986b9040a1cd03a345c6ecb05a2f1360be2
Han <hl2341@nyu.edu>
2022-09-19 17:59:27 -0400

    flavor texted

--- src/Main.js
+++ src/Main.js
@@ -12,10 +12,11 @@ const Main = () => {
         we have patients. We have providers. We serve static files through the
         native create-react-app public folder. We even have a fucking Contact
         page. Get the most out of your experience with Apple iHealthcare today.
-        We know you will truly love the experience.
+        We know you will truly love the experience. Make health as painless as using SQL
+        without Sequelize. With Apple iHealthcare, you can even center divs. That's fucking right.
       </p>
       <p>
-        Apple iHealthcare has you covered. Ask yourself: what can Apple do for
+        Apple iHealthcare has you covered. Ask yourself: what can Apple iHealthcare do for
         me?
         <ul>
           <li>
@@ -34,16 +35,34 @@ const Main = () => {
         </ul>
       </p>
       <em>
-        <h5>It's time to go green. Go to the Next Level. Go lang.</h5>{" "}
+        <h5>It's time to go Green. Go to the Next Level. Go Lang.</h5>{" "}
         <h4>
           <strong> GO APPLE iHEALTHCARE </strong>
         </h4>
       </em>
 
-      <p>
-        TESTING TIME.
+      <p><em>
+
+        Enjoy this shutterstock-free .jpeg resembling what appears to be a typical
+        turquoise-laden surgical team taken from an exceptionally awkward angle.
+        Here at Apple iHealthcare, we give you what you want, AND MORE.
+      </em>
+      <hr/>
         <img src={process.env.PUBLIC_URL + "/pic2.jpeg"} alt="Get_fucked" />
         {/* <img src={process.env.PUBLIC_URL + "/pic1.jpeg"} alt="Get_fucked"/> */}
+      </p>
+      <p>
+        Next time you think about hospitals, stethoscopes, diabetus, scurvy or dirty syringes,
+        we hope you can think of us instead. Enjoy the following misunderstood benefits:
+        <ul>
+            <li><strong>Going to receive a vasectomy anyways? Apply now and receive a BONUS pap smear during the same operation! </strong><br/> <small><em>Promotion is limited to one per customer.</em></small>  </li>
+            <li><strong>Want a regular check-up for your two prepubescent children? Order today for colonoscopy referral for whichever family member of your choosing!</strong> <br/> <small><em>We are not responsible for any forms of familial discord that erupts when you try to get your father-in-law a rectal exam</em></small>  </li>
+            <li><strong>Need a quick pick-me-up OTC for your seasonal allergies, cold, or flu? We have experimental monoclonal antibody angiotensin converting enzyme inhibitor antibiotic agents for NEW customers only!</strong><br/> <small><em>We do not take any liability for inevitable instances of iatrogenic MRSA.</em></small>  </li>
+        </ul>
+
+        <h3>Our team is happy to help you wherever, whenever, whatever, whomever, whyever. Apple iHealthcare </h3>
+
+        <img src={process.env.PUBLIC_URL + "/pic1.jpeg"} alt="Get_fucked" />
       </p>
     </div>
   );
