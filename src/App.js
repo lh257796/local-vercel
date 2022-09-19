5f7573b7c7e95c41320be7df647f595a8c5f05f1
Han <hl2341@nyu.edu>
2022-09-19 16:16:54 -0400

    3

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
         <Link to='/' className={pathname === '/' ? 'selected' : ''}> Main </Link>
         <Link to='/patients' className={pathname === '/patients' ? 'selected' : ''}> Patients </Link>
         <Link to='/providers' className={pathname === '/providers' ? 'selected' : ''}> Providers </Link>
-        <Link to='/diseases' className={pathname === '/diseases' ? 'selected' : ''}> Diseases </Link>
-
-
+        <Link to='/news' className={pathname === '/news' ? 'selected' : ''}> News </Link>
+        <Link to='/contact' className={pathname === '/contact' ? 'selected' : ''}> Contact Us </Link>
       </nav>
       <p>
-        Fucker.
+        TESTING TIME.
       <img src={process.env.PUBLIC_URL + "/pic2.jpeg"} alt="Get_fucked"/>
+      {/* <img src={process.env.PUBLIC_URL + "/pic1.jpeg"} alt="Get_fucked"/> */}
 
       </p>
     </div>
